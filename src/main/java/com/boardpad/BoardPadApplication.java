ec6a1b55273d883bf571f54a221dcd8ed66c23a7
Wellington Brito <wellington1000@yahoo.com.br>
2022-04-29 18:17:46 -0300

    primeiro commit

---
+++ src/main/java/com/boardpad/BoardPadApplication.java
@@ -0,0 +1,13 @@
+package com.boardpad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardPadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardPadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boardpad/BoardPadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardpad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardPadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
